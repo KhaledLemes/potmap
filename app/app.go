8944f24eff0ec6167382823075dcc8f95ebb024a
KhaledLemes <khaled.borges@outlook.com>
2025-11-07 18:33:44 -0300

    refactor: refined port scan
    - added populate function for struct scanResult
    - replaced urfave/cli with urfave/cli/v2
    - added aliases for all commands and flags
    - added new outputs in case all ports are closed and in case arguments are in an invalid format.

--- app/app.go
+++ app/app.go
@@ -3,12 +3,11 @@ package application
 import (
 	"errors"
 	"fmt"
-	"log"
 	"net"
 	"strconv"
 	"time"
 
-	"github.com/urfave/cli"
+	"github.com/urfave/cli/v2"
 )
 
 type scanResult struct {
@@ -25,41 +24,47 @@ type scanResult struct {
 var result = scanResult{}
 
 func Generate() *cli.App {
-	app := cli.NewApp()
-	app.Name = "potmap"
-	app.Usage = "A simple port scanner created by a simple student"
-
 	flags := []cli.Flag{
-		// Ip is the one that will be scanned
-		cli.StringFlag{
-			Name:  "ip",
-			Value: "127.0.0.1",
+		// Ip is the one that will be scanned. If no ip is declared, localhost will be scanned.
+		&cli.StringFlag{
+			Name:    "ip",
+			Aliases: []string{"Ip", "iP", "IP", "i"},
+			Value:   "127.0.0.1",
+		},
+		// Ports are the ports that will be scanned. If no port is declared, the most used TCP ports will be the ones scanned
+		&cli.StringSliceFlag{
+			Name:    "ports",
+			Aliases: []string{"p"},
+			Value:   cli.NewStringSlice("20", "21", "22", "25", "53", "80", "110", "143", "443", "3389", "8080"),
+		},
+		&cli.BoolFlag{
+			Name:    "ShowClosed",
+			Aliases: []string{"all", "seeall", "c", "sc", "showclosed", "show-closed"},
+			Value:   false,
 		},
 	}
 
-	app.Commands = []cli.Command{
-		{
-			Name:   "tq",
-			Usage:  "Does a port scan of the main TCP ports",
-			Flags:  flags,
-			Action: tQuickScan,
-		},
-		{
-			Name:   "uq",
-			Usage:  "Does a port scan of the main UDP ports",
-			Flags:  flags,
-			Action: uQuickScan,
+	app := cli.App{
+		Name:  "potmap",
+		Usage: "A simple port scanner created by a simple student",
+		Commands: []*cli.Command{
+			&cli.Command{
+				Name:    "Scan",
+				Aliases: []string{"scan", "S", "s"},
+				Usage:   "Does a TCP port scan of the main TCP ports. Also accepts port numbers as argument with flag --ports",
+				Flags:   flags,
+				Action:  Scan,
+			},
 		},
 	}
 
-	return app
+	return &app
 }
 
-func tQuickScan(c *cli.Context) {
-	result.ip = c.String("ip")
-	result.ports = []string{"20", "21", "22", "25", "53", "80", "110", "143", "443", "3389", "8080"}
-
-	result.scanned = uint(len(result.ports))
+func Scan(c *cli.Context) error {
+	if err := result.populate(c); err != nil {
+		return err
+	}
 
 	for _, port := range result.ports {
 		result.addr = fmt.Sprintf("%s:%s", result.ip, port)
@@ -67,14 +72,18 @@ func tQuickScan(c *cli.Context) {
 		conn, err := net.DialTimeout("tcp", result.addr, time.Second*3)
 		// Will check whether a connection is closed or filtered
 		if err != nil {
+			// If the error is a timeout, the port is filtered
 			var nerr net.Error
 			if errors.As(err, &nerr) && nerr.Timeout() {
-				fmt.Printf("Port %s is FILTERED\n", port)
+				fmt.Printf("Port %s is open | filtered\n", port)
 				result.filtered++
 				continue
 			}
 
-			fmt.Printf("Port %s is CLOSED\n", port)
+			// If user decided to keep closed hidden, it will not be printed
+			if c.Bool("ShowClosed") {
+				fmt.Printf("Port %s is CLOSED\n", port)
+			}
 			result.closed++
 			continue
 		}
@@ -84,57 +93,38 @@ func tQuickScan(c *cli.Context) {
 
 		conn.Close()
 	}
-
+	if result.open == 0 && result.filtered == 0 {
+		fmt.Println("------------------------------------------------")
+		fmt.Printf("All scanned ports for %s are closed :(\n", result.ip)
+		return nil
+	}
 	fmt.Println("------------------------------------------------")
 	fmt.Printf("Port scan for ip %s completed.\nScanned ports: %d\nOpen ports: %d\nFiltered ports: %d\nClosed ports: %d\n", result.ip, result.scanned, result.open, result.filtered, result.closed)
+	return nil
 }
 
-func uQuickScan(c *cli.Context) {
+func uScan(c *cli.Context) error {
+	result.populate(c)
+
+	return nil
+}
+
+// populate(c) populates result struct, which holds scan data.
+// It also validates the fields that can be altered by the user
+func (result *scanResult) populate(c *cli.Context) error {
+	// Populates and validade "ip" field
 	result.ip = c.String("ip")
-	result.ports = []string{"53", "67", "68", "69", "123", "161", "162", "500", "4500"}
-
-	result.scanned = uint(len(result.ports))
-
-	PacketConn, err := net.ListenPacket("udp", ":0")
-	if err != nil {
-		fmt.Println("The program failed to open a generic UDP connection.")
-		log.Fatal(err)
+	if net.ParseIP(result.ip) == nil {
+		return fmt.Errorf("\nInvalid IP format: %s", result.ip)
 	}
-	defer PacketConn.Close()
 
+	// Populates and validade "ports" field
+	result.ports = c.StringSlice("ports")
 	for _, port := range result.ports {
-		cport, err := strconv.ParseInt(port, 10, 16)
-		if err != nil {
-			fmt.Println("There was an error converting port to uint.")
-			log.Fatal(err)
-		}
-
-		// This part of the code writes to a destiny
-		dest := &net.UDPAddr{IP: net.ParseIP(result.ip), Port: int(cport)}
-		payload := []byte("WHOAREYOU")
-		_, err = PacketConn.WriteTo(payload, dest)
-
-		// Creates buffer
-		buffer := make([]byte, 4096)
-
-		// To avoid too much requests, the number of attempts is limited
-		const attempts = 3
-		for attempt := 1; attempt <= attempts; attempt++ {
-
-			// This part reads the response with a timeout
-			PacketConn.SetReadDeadline(time.Now().Add(2 * time.Second)) //Sets the timeout to 2 seconds from now
-
-			n, _, err := PacketConn.ReadFrom(buffer)
-			if err != nil {
-				var nerr net.Error
-				if errors.As(err, &nerr) && nerr.Timeout() {
-					if attempt < attempts {
-						time.Sleep(200 * time.Millisecond)
-						continue
-					}
-				}
-			}
-
+		if _, err := strconv.ParseUint(port, 10, 16); err != nil {
+			return fmt.Errorf("\n%s is not a valid port.", port)
 		}
 	}
+	result.scanned = uint(len(result.ports))
+	return nil
 }
